446f88a120228f96e73eca032afb357d51817ab4
Mohd Amir <Amirkhan98375@gmail.com>
2022-09-10 21:33:25 +0530

    done

---
+++ src/components/SelectComponent.jsx
@@ -0,0 +1,38 @@
+import React, { useEffect, useState } from 'react'
+import Select from 'multiselect-react-dropdown'
+import axios from 'axios'
+import './SelectComponent.css'
+
+export const SelectComponent = () => {
+  const [options,setoptions]=useState([])
+  const [data,setdata]=useState([])
+
+  useEffect(()=>{
+   const getCountry=async()=>{
+     let countrylist=[]
+        let res=await fetch(`https://apnastore123.herokuapp.com/country`)
+        let resdata= await res.json()
+        for(let i=0;i<resdata.length;i++){
+          countrylist.push(resdata[i].name)
+        }
+        console.log(countrylist)
+        setoptions(countrylist)
+   }
+   getCountry()
+  
+  },[])
+  return (
+    <div>
+     <div className='container'>
+     <Select 
+      placeholder='search country name'
+       select={"All"}
+      isObject={false} options={options} 
+      onSearch={true}
+      showCheckbox/>
+     </div>
+        
+   
+    </div>
+  )
+}
