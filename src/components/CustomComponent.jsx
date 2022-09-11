0a552dc1c006e03bd4b929de66e0baf6894a626a
Mohd Amir <Amirkhan98375@gmail.com>
2022-09-11 16:20:59 +0530

    searching add

--- src/components/CustomComponent.jsx
+++ src/components/CustomComponent.jsx
@@ -1,22 +1,30 @@
 import React, { useState } from 'react'
 import './Custom.css'
+import {IoIosRemove} from 'react-icons/io'
+import axios from 'axios'
+import { useEffect } from 'react'
+import { Box, Input } from '@chakra-ui/react'
 export const Select = () => {
     const [isvisible,setisvisible]=useState(true)
     const [newdata,setnewData]=useState([])
-    const [data,setdata]=useState([{
-        country:"india",
-
-        capital:"delhi"
-    },
-{
-    country:"pakistan",
-    capital:"Islamabad"
-},
-{
-    country:"bangladesh",
-    capital:"Dhaka"
+    const [final,setfinal]=useState([])
+    const [inputdata,setinput]=useState('')
+    const [data,setdata]=useState([])
+console.log(inputdata)
+const getData=(search)=>{
+    axios.get(`https://apnastore123.herokuapp.com/country`,{
+        params:{
+            q:search
+        }
+    })
+    .then((res)=>setdata(res.data))
 }
-])
+useEffect(()=>{
+getData(inputdata)
+},[inputdata])
+
+
+
 const handleAdd=(name)=>{
      let duplicate=newdata.includes(name)
     console.log(duplicate)
@@ -32,23 +40,39 @@ const handleAdd=(name)=>{
         
         var result = arrayRemove(newdata, name)
         setnewData(result)
+        
        }
+       
 }
-console.log(newdata)
+
 
 const [selectIndex,setselectIndex]=useState(null)
+
   return (
     <div>
     <div className='customSelect'>
-    <div className='showlist'>
-   {newdata}
-    </div>
+        
+    <Box border={"1px solid grey"} className='showlist'
+      placeholder='enter to searc'>
+      
+    {newdata?.map((el)=>{
+        return(
+           
+            <span className='span' >{el} <sup style={{backgroundColor:'whitesmoke'}}>x</sup>
+            
+            </span>
+        
+        )
+    })}
+     <Input width={"30%"} border={"none"} display={"hidden"}  placeholder={"search here"}
+     onChange={(e)=>setinput(e.target.value)}/>
+    </Box>
     <div className='select-item'>
         {data.map((el)=>{
             return(
                 <div className='checkboxshow'>
-                       <input  type="checkbox" className='checkbox'  onClick={()=>handleAdd(el.country)}/>
-                       {el.country}
+                       <input  type="checkbox" className='checkbox'  onClick={()=>handleAdd(el.name)}/>
+                       {el.name}
                  
                      
               
